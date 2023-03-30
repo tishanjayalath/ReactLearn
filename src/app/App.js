ef92a1d26ee72c758c6bccaf2906f75f2f241370
tishanjayalath <tishanjayalath18@gmail.com>
2023-03-30 23:22:34 +0530

    uploads

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import logo from '.C:\Users\Invora Technologies\Desktop\Reactjs\my-app\src\logo.svg/';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>thi is jshdjksdh hlkshd</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
